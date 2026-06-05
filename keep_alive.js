86a9590595ce4972413ab6ebc0e3d5019d26f7a8
Omar14231 <adsqwertt1@gmail.com>
2026-06-05 09:15:44 +0300

    Update server to use environment port or default to 8080

--- keep_alive.js
+++ keep_alive.js
@@ -3,7 +3,6 @@ const http = require('http');
 http.createServer((req, res) => {
   res.write("البوت يعمل 24/7");
   res.end();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
 
-console.log("نظام Keep Alive يعمل على البورت 8080");
-
+console.log("نظام Keep Alive يعمل بنجاح!");
