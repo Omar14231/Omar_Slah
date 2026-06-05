2e7b85027a34f25c2e968eb38acfff95f8ad5c00
Omar14231 <adsqwertt1@gmail.com>
2026-06-05 13:16:49 +0300

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.write("البوت يعمل 24/7");
-  res.end();
-}).listen(process.env.PORT || 8080);
-
-console.log("نظام Keep Alive يعمل بنجاح!");
